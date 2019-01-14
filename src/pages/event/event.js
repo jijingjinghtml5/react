3c98a4d8b909ab8fd3ab2fde7fe8ffbbf41e2137
jijingjing <jijingjing@vmchop.com>
2019-01-14 19:03:12 +0800

    活动

--- src/pages/event/event.js
+++ src/pages/event/event.js
@@ -4,11 +4,19 @@ import {NavLink as Link } from 'react-router-dom';
 import { Header,Footer } from '../../component/common';
 import PropTypes from 'prop-types';
 import { Tool,Util } from '../../util';
+import * as config from '../../Config/Config';
 // import './activity.less';
 
 class Activity extends Component {
     constructor(){
         super();
+    }
+    componentWillMount(){
+        console.log(config);
+        this.setState({
+            img_url:config,
+            images:{}
+        })
     }
     componentDidMount() {
         let _this = this;
@@ -30,9 +38,30 @@ class Activity extends Component {
         console.log(res);
         this.setState(res);
     }
+    //加载图片
+    loadImage(image_id){
+        Util.loadImage(this,image_id, 'm');
+        window.dispatchEvent(new Event('resize'));
+        this.setState({ imgHeight: 'auto' });
+    }
     render(){
+        console.log(this.state);
+        if(!this.state.activity) return null;
+        let images = this.state.images;
+        if(!images){
+            images = [];
+        }
         return (
-            <ul></ul>
+            <div>
+                {/*活动专场头部*/}
+                <div className={"brand-top"}  style={{backgroundImage:'url('+this.state.img_url+'/public/wechat/active/brand_bg.png)'}}>
+                    <img src={images[this.state.activity.merchant.logo_image]?images[this.state.activity.merchant.logo_image]:'data:image/gif;base64,R0lGODlhAQABAIAAAO/v7////yH5BAAHAP8ALAAAAAABAAEAAAICRAEAOw=='} onLoad={this.loadImage.bind(this,this.state.activity.merchant.logo_image)}/>
+                    <div className="brand-title">{this.state.activity.merchant.show_name}</div>
+                    {/*<div className="brand-desc">{this.state.activity.goods_count ? this.state.activity.goods_count : 0}款商品<label*/}
+                        {/*wx:if="{{activity.mtag}}">{this.state.activity.mtag}</label></div>*/}
+                </div>
+            </div>
         )
     }
 }
+export default Activity;
